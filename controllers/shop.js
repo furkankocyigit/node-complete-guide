d579c9b3275057a419f48a6ae8adc064a4a70a62
furkan kocyigit <furkankocyigit58@gmail.com>
2023-03-30 14:53:03 +0300

    pagination logic is added.

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,14 +5,28 @@ const order = require('../models/order')
 const fs = require('fs')
 const path = require('path')
 const PDFDocument = require('pdfkit')
-
+const ITEMS_PER_PAGE = 4;
 exports.getIndex = (req,res,next) =>{
-    Product.find()
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.find().countDocuments()
+    .then(numProducts =>{
+        totalItems = numProducts;
+        return Product.find()
+            .skip((page - 1) *ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE);
+    }) 
     .then(products => {
         res.render('shop/index',{
             prods:products,
             pageTitle:'Shop', 
             path:'/',
+            currentPage:page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page +1,
+            previousPage: page -1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
         })
     })
     .catch(err => {
@@ -20,18 +34,33 @@ exports.getIndex = (req,res,next) =>{
         error.httpStatusCode = 500;
         return next(error)
     }) 
+   
+   
 }
 
 exports.getProducts = (req,res,next) =>{
-    Product.find()
-        .then(products => {
-            res.render('shop/product-list',{
-                prods:products,
-                pageTitle:'All Products', 
-                path:'/products',
-            })
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.find().countDocuments()
+    .then(numProducts =>{
+        totalItems = numProducts;
+        return Product.find()
+            .skip((page - 1) *ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE);
+    }) 
+    .then(products => {
+        res.render('shop/product-list',{
+            prods:products,
+            pageTitle:'All Products', 
+            path:'/products',
+            currentPage:page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page +1,
+            previousPage: page -1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
         })
-        .catch(err => {
+    }).catch(err => {
             const error = new Error(err)
             error.httpStatusCode = 500;
             return next(error)
@@ -142,7 +171,7 @@ exports.getOrders = (req,res,next) =>{
                 orders: orders,
             })
 
-        })
+    })
         .catch(err => {
             const error = new Error(err)
             error.httpStatusCode = 500;
